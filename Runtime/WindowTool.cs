014b98412bb483d46f562a5b85802cd7289a38e7
s_xgx1 <S_xgx1@outlook.com>
2024-07-03 18:37:57 +0800

    fix: WindowTool.cs utf8

--- Runtime/WindowTool.cs
+++ Runtime/WindowTool.cs
@@ -6,8 +6,8 @@ namespace PunctualSolutionsTool.Tool
 {
     public static class WindowTool
     {
-        [DllImport("user32.dll")]
-        private static extern IntPtr FindWindow(string lpClassName, string lpWindowName);
+        [DllImport("user32.dll", EntryPoint = "FindWindowW", CharSet = CharSet.Unicode)]
+        private static extern IntPtr FindWindowW(string lpClassName, string lpWindowName);
 
         private const uint SwpShowWindow = 0x0040;
 
@@ -16,7 +16,7 @@ namespace PunctualSolutionsTool.Tool
 
         public static void SetResolution(int width, int height, int posX = 0, int posY = 0)
         {
-            var hWnd = FindWindow(null, Application.productName);
+            var hWnd = FindWindowW(null, Application.productName);
             if (hWnd == IntPtr.Zero) return;
             SetWindowPos(hWnd, 0, posX, posY, width, height, SwpShowWindow);
         }
