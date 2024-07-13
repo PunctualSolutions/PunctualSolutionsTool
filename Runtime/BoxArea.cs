0bcb8d207a98b3a15da016c4783348bd055f3d4d
s_xgx1 <S_xgx1@outlook.com>
2024-07-13 12:44:56 +0800

    add BoxArea

---
+++ Runtime/BoxArea.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+namespace AutoLive.Main
+{
+    public class BoxArea : MonoBehaviour
+    {
+        [field: SerializeField] public Vector2 SpawnRange { get; private set; }
+
+        void OnDrawGizmos()
+        {
+            Gizmos.color = Color.yellow;
+            Gizmos.DrawWireCube(transform.position, new(SpawnRange.x, 0, SpawnRange.y));
+        }
+    }
+}
