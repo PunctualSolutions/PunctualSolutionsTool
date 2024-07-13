82a72166b014887bc0f0079e75ee9bdd97cfd7dd
s_xgx1 <S_xgx1@outlook.com>
2024-07-13 12:56:32 +0800

    fix: remove space

--- Runtime/BoxArea.cs
+++ Runtime/BoxArea.cs
@@ -19,22 +19,16 @@ namespace AutoLive.Main
             var maxTries = 10 * count;
             var points   = new List<Vector2>();
             var tries    = 0;
-
             while (points.Count < count && tries < maxTries)
             {
                 var randomPoint = new Vector2(Random.Range(range.x, range.y), Random.Range(range.x, range.y));
                 if (IsPointValid(points, randomPoint, minDist)) points.Add(randomPoint);
                 tries++;
             }
-
             if (points.Count < count) throw new("无法在给定范围内生成足够数量的点");
-
             return points;
         }
 
-        static bool IsPointValid(List<Vector2> points, Vector2 point, float minDist)
-        {
-            return points.All(otherPoint => !(Vector2.Distance(point, otherPoint) < minDist));
-        }
+        static bool IsPointValid(List<Vector2> points, Vector2 point, float minDist) => points.All(otherPoint => !(Vector2.Distance(point, otherPoint) < minDist));
     }
 }
