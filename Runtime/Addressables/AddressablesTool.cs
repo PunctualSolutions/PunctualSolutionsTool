e0c2390eb9d84ca68a393c79f94c05c32270cdbd
s_xgx1 <S_xgx1@outlook.com>
2024-08-04 09:59:50 +0800

    fix: add Get

--- Runtime/Addressables/AddressablesTool.cs
+++ Runtime/Addressables/AddressablesTool.cs
@@ -4,12 +4,15 @@ using System.Linq;
 using Cysharp.Threading.Tasks;
 using UnityEngine;
 using UnityEngine.AddressableAssets;
+using UnityEngine.ResourceManagement.AsyncOperations;
+using UAddressable = UnityEngine.AddressableAssets.Addressables;
 
 namespace PunctualSolutions.Tool.Addressables
 {
     public static class AddressablesTool
     {
-        static readonly List<AssetReference> WaitAsset = new();
+        static readonly List<AssetReference> WaitAsset       = new();
+        static readonly List<string>         WaitAssetString = new();
 
         public static async UniTask<T> Get<T>(this AssetReferenceT<T> value) where T : Object
         {
@@ -24,8 +27,17 @@ namespace PunctualSolutions.Tool.Addressables
             while (WaitAsset.First() != value)
                 await UniTask.NextFrame();
             WaitAsset.Remove(value);
-            var loadAssetAsync = await value.LoadAssetAsync();
-            return loadAssetAsync;
+            return await value.LoadAssetAsync();
+        }
+
+        public static async UniTask<T> Get<T>(string key) where T : Object
+        {
+            WaitAssetString.Add(key);
+            await UniTask.NextFrame();
+            while (WaitAssetString.First() != key)
+                await UniTask.NextFrame();
+            WaitAssetString.Remove(key);
+            return await UAddressable.LoadAssetAsync<T>(key);
         }
     }
 }
