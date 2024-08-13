21a5803e2820b6f7cd069ec95c1aca7031c24be3
s_xgx1 <S_xgx1@outlook.com>
2024-08-13 21:52:48 +0800

    fix: add MonoAutoSingleton

---
+++ Runtime/MonoAutoSingleton.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+
+namespace PunctualSolutions.RideBicycle
+{
+    public class MonoAutoSingleton<T> : MonoBehaviour where T : MonoBehaviour
+    {
+        static T _instance;
+
+        public static T Instance
+        {
+            get
+            {
+                _instance ??= new GameObject(typeof(T).Name).AddComponent<T>();
+                return _instance;
+            }
+            private set => _instance = value;
+        }
+
+        protected virtual bool IsDontDestroyOnLoad => true;
+    }
+}
